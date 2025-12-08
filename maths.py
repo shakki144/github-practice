40c51551aa4121e0c116634f9d9bb33bc5d1eb7a
dona <donaeapen@gmail.com>
2025-12-08 19:36:05 +0530

    substraction multiplication function added

--- maths.py
+++ maths.py
@@ -1,5 +1,17 @@
 def add(a,b):
   result = a+b
   print(result)
-  return a+b
+  return result
+def subtract(a,b):
+  result = a - b
+  print("Subtraction:", result)
+  return result
+
+def multiply(a,b):
+  result = a * b
+  print("Multiplication:", result)
+  return result
+  
 add (5,3)
+subtract(10, 5)
+multiply(10, 5)
